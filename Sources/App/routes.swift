fd407c0c160b72b65edf7e5e49aef54437ce08d9
Maksim Volkov <maksan.russ@mail.ru>
2025-06-22 13:12:51 +0300

    Is changed of the hello message and is added the status page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+        return req.view.render("index", ["title": "Серверная часть проекта Shop"])
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("status") { req -> String in
+        return "Серверная часть проекта Shop запущена и доступна ✅"
     }
 }
