b05e660f17a43809334337cc8c9d4a602dbaffda
Maksim Volkov <maksan.russ@mail.ru>
2025-06-22 13:01:20 +0300

    Switch to port 8086

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,11 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    //    меняем хост
+    //    app.http.server.configuration.hostname = "domen or IP"
+    //    меняем номер порта
+    app.http.server.configuration.port = 8086
+    
     app.views.use(.leaf)
 
 
