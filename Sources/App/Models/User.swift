0dc834b0da1753a64546f5c4cb76708d3f9bc1cf
Maksim Volkov <maksan.russ@mail.ru>
2025-06-28 10:51:09 +0300

    Create model of user and model of users message

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksim Volkov on 28.06.2025.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var email: String
+    var password: String
+    var credit_card: String
+    var itemsInCart: [String]?
+    var favourites: [String]?
+}
